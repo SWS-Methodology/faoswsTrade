9f4cf83fdaf7479a6993c3966d96b8cf6ea4c45c
Alexander Matrunich <alexander@matrunich.com>
2015-09-24 16:39:04 +0200

    stringsAsFactors = F in comtradeunits.RData

--- data-raw/comtrade_units.R
+++ data-raw/comtrade_units.R
@@ -1,4 +1,5 @@
-comtradeunits <- read.csv("data-raw/comtrade_units.csv", header = F)
+comtradeunits <- read.csv("data-raw/comtrade_units.csv", header = F,
+                          stringsAsFactors = F)
 colnames(comtradeunits) <- c("qunit", "wco", "desc", "faoconv")
 comtradeunits$faoconv <- NULL
 save(comtradeunits, file = "data/comtradeunits.RData")

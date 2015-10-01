1868c991c085615e1fec3b6c083c61e40387fe07
Alexander Matrunich <alexander@matrunich.com>
2015-10-01 12:12:32 +0200

    General function for reading tables from MDB files created

---
+++ vignettes/readMDBTAble.R
@@ -0,0 +1,37 @@
+readMDBTable <- function(area,
+                         year,
+                         table,
+                         mdbdir = c("",
+                                    "mnt",
+                                    "essdata",
+                                    "TradeSys",
+                                    "TradeSys",
+                                    "Countries"),
+                         colClasses = "character") {
+  area <- toupper(area)
+
+  if(length(mdbdir) > 1) mdbdir <- do.call("file.path", as.list(mdbdir))
+
+  dirname <- paste0(area, "_", year)
+
+  path <- file.path(mdbdir, dirname)
+
+  if(!file.exists(path)) stop(paste0("Directory ",
+                                     path,
+                                     " does not exist."))
+
+  filepath <- file.path(path, paste0(dirname, ".mdb"))
+
+  if(!file.exists(filepath)) stop(paste0("File ",
+                                     filepath,
+                                     " does not exist."))
+
+
+  Hmisc::mdb.get(filepath,
+                 colClasses = "character",
+                 stringsAsFactors = F,
+                 lowernames = T,
+                 tables = table)
+
+
+}

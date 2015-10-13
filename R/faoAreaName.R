b5d084106774885d373e3e559f8f7eda3ac51f9b
Alexander Matrunich <alexander@matrunich.com>
2015-10-13 16:24:47 +0200

    faoAreaname() created

---
+++ R/faoAreaName.R
@@ -0,0 +1,25 @@
+#' @import dplyr
+#' @export
+
+faoAreaName <- function(areacode,
+                    swsdomain = "faostatOne",
+                    swsdataset = "faostatOne",
+                    swsdimension = "geographicAreaFS") {
+
+
+  if (!is.integer(areacode))
+    areacode <- as.integer(areacode)
+
+  # It is possible to ask SWS on specific codes, but if it is absent, you
+  # get a 500 error from the server. So it is easier to get full set
+  data.frame(areacode) %>%
+    left_join(faosws::GetCodeList(swsdomain, swsdataset, swsdimension) %>%
+                as.data.frame() %>%
+                select_(~code, ~description) %>%
+                mutate_(code = ~as.integer(code)),
+              by = c("areacode" = "code")) %>%
+    select_(~description) %>%
+    unlist() %>%
+    unname()
+
+}

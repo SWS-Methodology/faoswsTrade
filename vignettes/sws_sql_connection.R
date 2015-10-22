cbb25cef746cd0f2106c284ac4cf25ea709c692f
Alexander Matrunich <alexander@matrunich.com>
2015-10-22 17:05:48 +0200

    initial commit of it

---
+++ vignettes/sws_sql_connection.R
@@ -0,0 +1,17 @@
+.pwd <- "rUikJnLmtDN7iA5c7u06"
+
+trade_src <- src_postgres(dbname = "sws_data",
+                          host = "hqldvpostgres01.hq.un.fao.org",
+                          port = 5420,
+                          user = "sws",
+                          password = .pwd)
+
+# It is required for table view
+# ,
+#                           options = "-c search_path=ess")
+
+agri_db <- tbl(trade_src, sql("
+                              select * from ess.ce_combinednomenclature_unlogged
+                              limit 10"))
+
+

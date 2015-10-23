9a303878e748324990a9a76c93afbdec041d97fe
Alexander Matrunich <alexander@matrunich.com>
2015-10-23 16:30:32 +0200

    getTableFromDB() added

---
+++ R/getTableFromDB.R
@@ -0,0 +1,34 @@
+#' @import dplyr
+#' @export
+
+getTableFromDB <- function(
+  sqlrequest,
+  dbname = "sws_data",
+  host = "hqldvpostgres01.hq.un.fao.org",
+  port = 5420,
+  user = "sws",
+  password,
+  options = "-c search_path=ess") {
+
+  if(missing(password) & !exists(".swsdbpwd"))
+    stop("Please provide password to db: as argument either create .swsdbpwd variable")
+
+  if(missing(password) & exists(".swsdbpwd"))
+    password <- .swsdbpwd
+
+  sws_src <- src_postgres(dbname = dbname,
+                            host = host,
+                            port = port,
+                            user = user,
+                            password = password,
+                            options = options)
+
+  tbl_src <- tbl(trade_src, sql(sqlrequest))
+
+  localtbl <- collect(tbl_src)
+
+  RODBC::odbcClose(tbl_src$con)
+
+  localtbl
+
+}

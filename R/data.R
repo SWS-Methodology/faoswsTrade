cde013f300965004575b5eb3bc3bb519d7917eda
Alexander Matrunich <alexander@matrunich.com>
2015-10-29 15:26:04 +0100

    geonom2fao dataset added

--- R/data.R
+++ R/data.R
@@ -33,3 +33,18 @@
 #' desc (character) - Description
 
 "comtradeunits"
+
+#' Eurostat Geonomenclature to FAO area codes mapping
+#'
+#' Extracted from CountryCode table of FRA_2012.mdb located in
+#'  /mnt/essdata/TradeSys/TradeSys/Countries/FRA_2012/
+#' All EU countries have similar codes. So we took France.
+#' Idea suggested by Claudia
+#'
+#' @format A data frame: code (integer) Geonom code,
+#' faostat (integer) - FAO area code,
+#' active (integer) - FAO area code to use. For example, French Guiana
+#' goes to France. So we don't need to aggregate later (probably). Abandoned codes,
+#' like USSR, in active column were equal 0, but replaced by NA.
+
+"geonom2fao"

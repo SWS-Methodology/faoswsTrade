5b46482eba083173c2eec23156300eb6b11e8685
Alexander Matrunich <alexander@matrunich.com>
2015-12-03 15:53:15 +0100

    filtering of hsfclmap moved to place where it is firstly required

--- vignettes/producingSet.R
+++ vignettes/producingSet.R
@@ -48,30 +48,6 @@ data("unsdpartnersblocks", package = "tradeproc", envir = environment())
 data("unsdpartners", package = "tradeproc", envir = environment())
 
 
-# ---- hsfclmapsubset ----
-######### HS -> FCL map ############
-## Filter hs->fcl links we need (based on year)
-
-hsfclmap <- hsfclmap2 %>%
-  # Filter out all records from future years
-  filter_(~mdbyear <= year) %>%
-  # Distance from year of interest to year in the map
-  mutate_(yeardistance = ~year - mdbyear) %>%
-  # Select nearest year for every reporter
-  # if year == 2011 and mdbyear == 2011, then distance is 0
-  # if year == 2011 and mdbyear == 2010, distance is 1
-  group_by_(~area) %>%
-  filter_(~yeardistance == min(yeardistance)) %>%
-  ungroup() %>%
-  select_(~-yeardistance) %>%
-  ## and add trailing 9 to tocode, where it is shorter
-  ## TODO: check how many such cases and, if possible, move to manualCorrectoins
-  mutate_(tocode = ~hsfclmap::trailingDigits(fromcode,
-                                           tocode,
-                                           digit = 9))
-
-
-
 # ---- tradeload ----
 
 #### Get list of agri codes ####
@@ -133,6 +109,29 @@ tldata <- tldata %>%
               select_(~reporter) %>%
               distinct(),
             by = "reporter")
+
+
+# ---- hsfclmapsubset ----
+# HS -> FCL map
+## Filter hs->fcl links we need (based on year)
+
+hsfclmap <- hsfclmap2 %>%
+  # Filter out all records from future years
+  filter_(~mdbyear <= year) %>%
+  # Distance from year of interest to year in the map
+  mutate_(yeardistance = ~year - mdbyear) %>%
+  # Select nearest year for every reporter
+  # if year == 2011 and mdbyear == 2011, then distance is 0
+  # if year == 2011 and mdbyear == 2010, distance is 1
+  group_by_(~area) %>%
+  filter_(~yeardistance == min(yeardistance)) %>%
+  ungroup() %>%
+  select_(~-yeardistance) %>%
+  ## and add trailing 9 to tocode, where it is shorter
+  ## TODO: check how many such cases and, if possible, move to manualCorrectoins
+  mutate_(tocode = ~hsfclmap::trailingDigits(fromcode,
+                                           tocode,
+                                           digit = 9))
 
 # ---- drop_reps_not_in_mdb ----
 # We drop reporters what are absent in MDB hsfcl map

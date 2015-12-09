92a39ee14979937a70f915720f4bf68f441c19d4
Alexander Matrunich <alexander@matrunich.com>
2015-12-09 11:46:29 +0100

    descFCL without SWS dependency

--- R/descFCL.R
+++ R/descFCL.R
@@ -10,10 +10,16 @@ descFCL <- function(item,
   if (!is.integer(item))
     item <- as.integer(item)
 
+  if(is.SWSEnvir()) {
+    fclcodelist <- faosws::GetCodeList(swsdomain,
+                                       swsdataset,
+                                       swsdimension)
+  } else data("fclcodelist", package = "tradeproc", envir = environment())
+
   # It is possible to ask SWS on specific codes, but if it is absent, you
   # get a 500 error from the server. So it is easier to get full set
   data.frame(item) %>%
-    left_join(faosws::GetCodeList(swsdomain, swsdataset, swsdimension) %>%
+    left_join(fclcodelist %>%
                 as.data.frame() %>%
                 select_(~code, ~description) %>%
                 mutate_(code = ~as.integer(code)),

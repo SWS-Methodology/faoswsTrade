031f4fa2899f4e6f1e0960d821462955798b217d
marcogarieri <marcogarieri@gmail.com>
2016-06-02 11:56:38 +0200

    Removing unsdpartners, since not necessary. Fix #21

--- R/data.R
+++ R/data.R
@@ -1,11 +1,3 @@
-#' UNSD partner reference list
-#'
-#' Taken from Melissa Paca paca@un.org
-#'
-#' @format A data frame
-
-"unsdpartners"
-
 #' UNSD partner blocksreference list
 #'
 #' Taken from Melissa Paca paca@un.org

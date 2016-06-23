45fddc3ba1c7da9aad96811c21a9767be5dda0b7
marcogarieri <marcogarieri@gmail.com>
2016-06-23 20:19:20 +0200

    This should work in order to update the new fcl2cpc_ver_2_1 table, related to issue #29. Please Sebastian try on the SWS in order to update the table

--- data-raw/fcl_2_cpc2.R
+++ data-raw/fcl_2_cpc2.R
@@ -4,17 +4,16 @@ fcl_2_cpc2_complete <- XLConnect::readWorksheetFromFile("data-raw/Conv_3DEC2015_
 final_destination = "fcl2cpc_ver_2_1"
 table <- ReadDatatable(final_destination, readOnly = F)
 
+colnames(fcl_2_cpc2_complete) <- c("fcl", "fcl_cpc_parlink", "cpc",
+                                   "cpc_fcl_parlink", "description",
+                                   "suafbs_convfact", "suafbs_notes",
+                                   "fclcpc_convfact", "production_notes")
+
+fcl_2_cpc2_complete$fcl[grep("n/a",fcl_2_cpc2_complete$fcl)] <- NA
+
 changeset <- Changeset(final_destination)
 AddDeletions(changeset, table)
 Finalise(changeset)
 
 AddInsertions(changeset, fcl_2_cpc2_complete)
 Finalise(changeset)
-
-
-#fcl_2_cpc2 <- fcl_2_cpc2_complete[, colnames(fcl_2_cpc2_complete)[c(1,3)]]
-#colnames(fcl_2_cpc2) <- c("fcl","cpc")
-#fcl_2_cpc2$fcl[grepl("n/a",fcl_2_cpc2$fcl)] <- NA
-
-#write.csv(fcl_2_cpc2, file = "fcl_2_cpc2.csv", quote = T, row.names = F)
-#save(fcl_2_cpc2, file = "data/fcl_2_cpc2.RData")

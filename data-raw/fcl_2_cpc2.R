9f13b0f7dc0929e5d44a9a5bc0313d858d3b2810
Sebastian Campbell <sebastian.campbell@fao.org>
2016-06-24 11:10:35 +0200

    Added documentation to data-raw/fcl_2_cpc2.R and expanded NA replacement

--- data-raw/fcl_2_cpc2.R
+++ data-raw/fcl_2_cpc2.R
@@ -1,19 +1,27 @@
+# NOTE: faosws required and a session already established
+
+# Read in new, corrected cpc conversion file
 fcl_2_cpc2_complete <- XLConnect::readWorksheetFromFile("data-raw/Conv_3DEC2015_E1_Simplified.xlsx",
                                                         sheet = "Sheet3")
 
-final_destination = "fcl2cpc_ver_2_1"
-table <- ReadDatatable(final_destination, readOnly = F)
-
+# Convert to code on SWS system
 colnames(fcl_2_cpc2_complete) <- c("fcl", "fcl_cpc_parlink", "cpc",
                                    "cpc_fcl_parlink", "description",
                                    "suafbs_convfact", "suafbs_notes",
                                    "fclcpc_convfact", "production_notes")
 
-fcl_2_cpc2_complete$fcl[grep("n/a",fcl_2_cpc2_complete$fcl)] <- NA
+# Convert code meaning NA to NA
+fcl_2_cpc2_complete$fcl[grep("n/a", fcl_2_cpc2_complete$fcl, ignore.case = TRUE)] <- NA
 
+# Read in old file
+final_destination = "fcl2cpc_ver_2_1"
+table <- ReadDatatable(final_destination, readOnly = F)
+
+# Delete all old values in table
 changeset <- Changeset(final_destination)
 AddDeletions(changeset, table)
 Finalise(changeset)
 
+# Replace with new values
 AddInsertions(changeset, fcl_2_cpc2_complete)
 Finalise(changeset)

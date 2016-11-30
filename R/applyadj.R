d1136cdc978a3bb7fa0bc98c1bbaf5a7b2fdc5e4
Christian Mongeau <chrMongeau@users.noreply.github.com>
2016-11-30 17:54:56 +0100

    Add variable with adjustments to tradedata in debug mode

--- R/applyadj.R
+++ R/applyadj.R
@@ -45,6 +45,18 @@ applyadj <- function(rep, yr, adjustments, tradedata, dbg = FALSE) {
     if(dbg) matching_adjustments[,i] <<- tradedata$applyrule
   }
 
+  # The instructions below will add a column to `tradedata` with all the adjustments applied
+  # to a single record separated by ' @ ' (each adjustments is collapsed by '|')
+  # Example (two adjustments):
+  # 2011|2|21021010|1232|29|value|NA|NA|0.05443361|150 @ 2011|2|21021010|1232|NA|value|NA|NA|0.05443361|150
+  #
+  # WARNING: This is used (and always should be used) only in debug mode as it slows the module.
+  if (dbg) {
+    adj <- attributes(adjustments)$split_labels
+    tradedata$adjustments <- apply(matching_adjustments, 1, function(x) paste(apply(adj[x,], 1, paste, collapse='|'), collapse=' @ '))
+  }
+
+
   tradedata
 
 

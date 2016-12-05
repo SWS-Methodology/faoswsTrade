43857fef616e4e1d79919c4320a47eb012dfcd35
Christian Mongeau <chrMongeau@users.noreply.github.com>
2016-12-05 12:26:10 +0100

    Use HS8 and HS6 in imputation

--- R/doImputation.R
+++ R/doImputation.R
@@ -3,8 +3,10 @@
 #' Use a median unit value to impute missing quantities or outliers.
 #'
 #' The median unit value is calculated in a specific-to-generic fashion:
-#' 1) by HS-reporter; 2) by FCL-reporter; 3) by HS; 4) by FCL; 5) by flow.
-#' The first available of these options will be used.
+#' 1) by HS-reporter; 2) by HS8-reporter; by HS6-reporter; 3) by HS;
+#' 4) by FCL; 5) by flow.
+#' The first available of these options will be used (for HS, HS8 and HS6
+#' only when there are at least 10 flows).
 #'
 #' @param tradedata Trade data.
 #' @return \code{tradedata} with imputed values.
@@ -16,17 +18,26 @@ doImputation <- function(tradedata = NA) {
   if (missing(tradedata)) stop('"tradedata" is missing.')
 
   tradedata <- tradedata %>%
+    mutate(i=1, hs8 = str_sub(hs, 1, 8), hs6 = str_sub(hs, 1, 6)) %>%
+    mutate(
+           hs8 = if_else(nchar(hs8)==7, paste0('0', hs8), hs8),
+           hs6 = if_else(nchar(hs6)==5, paste0('0', hs6), hs6)
+           ) %>%
     # median value reporter/HS
     group_by_(~year, ~reporter, ~flow, ~hs) %>%
-    mutate_(uv_reporter_hs = ~median(uv, na.rm = TRUE)) %>%
+    mutate_(n_reporter_hs = ~sum(i), uv_reporter_hs = ~median(uv, na.rm = TRUE)) %>%
     ungroup() %>%
-    # median value reporter/FCL
-    group_by_(~year, ~reporter, ~flow, ~fcl) %>%
-    mutate_(uv_reporter_fcl = ~median(uv, na.rm = TRUE)) %>%
+    # median value reporter/HS8
+    group_by_(~year, ~reporter, ~flow, ~hs8) %>%
+    mutate_(n_reporter_hs8 = ~sum(i), uv_reporter_hs8 = ~median(uv, na.rm = TRUE)) %>%
+    ungroup() %>%
+    # median value reporter/HS6
+    group_by_(~year, ~reporter, ~flow, ~hs6) %>%
+    mutate_(n_reporter_hs6 = ~sum(i), uv_reporter_hs6 = ~median(uv, na.rm = TRUE)) %>%
     ungroup() %>%
     # median value HS
     group_by_(~year, ~flow, ~hs) %>%
-    mutate_(uv_hs = ~median(uv, na.rm = TRUE)) %>%
+    mutate_(n_hs = ~sum(i), uv_hs = ~median(uv, na.rm = TRUE)) %>%
     ungroup() %>%
     # median value FCL
     group_by_(~year, ~flow, ~fcl) %>%
@@ -38,17 +49,17 @@ doImputation <- function(tradedata = NA) {
     ungroup()
 
   imputed_qty <- case_when(
-    !is.na(tradedata$uv_reporter_hs)  ~ tradedata$value / tradedata$uv_reporter_hs,
-    !is.na(tradedata$uv_reporter_fcl) ~ tradedata$value / tradedata$uv_reporter_fcl,
-    !is.na(tradedata$uv_hs)           ~ tradedata$value / tradedata$uv_hs,
-    !is.na(tradedata$uv_fcl)          ~ tradedata$value / tradedata$uv_fcl,
+    !is.na(tradedata$uv_reporter_hs)  & tradedata$n_reporter_hs  > 10  ~ tradedata$value / tradedata$uv_reporter_hs,
+    !is.na(tradedata$uv_reporter_hs8) & tradedata$n_reporter_hs8 > 10  ~ tradedata$value / tradedata$uv_reporter_hs8,
+    !is.na(tradedata$uv_reporter_hs6) & tradedata$n_reporter_hs6 > 10  ~ tradedata$value / tradedata$uv_reporter_hs6,
+    !is.na(tradedata$uv_hs)                                            ~ tradedata$value / tradedata$uv_hs,
+    !is.na(tradedata$uv_fcl)                                           ~ tradedata$value / tradedata$uv_fcl,
     # XXX
-    !is.na(tradedata$uv_flow)         ~ tradedata$value / tradedata$uv_flow
+    !is.na(tradedata$uv_flow)                                          ~ tradedata$value / tradedata$uv_flow
   )
 
-  tradedata <- tradedata %>%
+  tradedata %>%
     mutate_(qty = ~ifelse(no_quant | outlier, imputed_qty, qty),
            flagTrade = ~ifelse(no_quant | outlier, 1, 0))
 
 }
-

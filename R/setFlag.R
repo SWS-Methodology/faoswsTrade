4e8deb42b26435fe08527bb72288b27ddf00894b
Christian Mongeau <chrMongeau@users.noreply.github.com>
2017-01-06 11:59:04 +0100

    Rethink names of parameters

--- R/setFlag.R
+++ R/setFlag.R
@@ -1,6 +1,6 @@
 #' Set flags in trade data.
 #'
-#' @param type The type of flag to set: "observation" or "method".
+#' @param type The type of flag to set: "status" or "method".
 #' @param flag The value of the flag to set (e.g., "e" for estimated data).
 #' @param variable The variable that gets the flag: "value", "weight",
 #'   "quantity".
@@ -10,13 +10,13 @@
 
 setFlag <- function(type = NA, flag = NA, variable = NA) {
 
-  if (missing(type) | !(type %in% c('value', 'quantity', 'weight'))) {
-    stop("'type' should be one of c('value', 'quantity', 'weight').")
+  if (missing(type) | !(type %in% c('value', 'quantity', 'weight', 'all'))) {
+    stop("'type' should be one of c('value', 'quantity', 'weight', 'all').")
   }
 
   if (missing(flag)) {
     stop("Please, set 'flag' to one flag value.")
-  } else if (type == 'observation' & !(flag %in% c('E', 'I', 'X'))) {
+  } else if (type == 'status' & !(flag %in% c('E', 'I', 'X'))) {
     stop(paste("'flag' should be one of 'E', 'I', 'X'"))
   } else if (type == 'method' & !(flag %in% c('c', 'i', 'e', 's', 'h'))) {
     stop(paste("'flag' should be one of 'c', 'i', 'e', 's', 'h'"))
@@ -26,6 +26,8 @@ setFlag <- function(type = NA, flag = NA, variable = NA) {
     stop("Please, set 'variable' to 'value', 'quantity', or 'weight'")
   }
 
-  if (type == 'observation') flag <- paste0('flag_status_', toupper(flag))
+  if (type == 'status') flag <- paste0('flag_status_', flag)
+
+  if (type == 'method') flag <- paste0('flag_method_', flag)
 
 }

ac02a547657b3c5b6d0aa89d023d964d84f2a0b7
Christian Mongeau <chrMongeau@users.noreply.github.com>
2017-01-07 12:24:30 +0100

    Modified the adjustments function

--- R/useAdjustments.R
+++ R/useAdjustments.R
@@ -39,12 +39,30 @@ useAdjustments <- function(tradedata = NA,
     message(sprintf("[%s] Applying adjustments to %s", PID, d))
   }
 
-  tbl_df(plyr::ldply(sort(unique(tradedata$reporter)),
+  # Needed, because of multiple rows
+  tradedata$key <- 1:nrow(tradedata)
+
+  tradedata_adj <- tbl_df(plyr::ldply(sort(unique(tradedata$reporter)),
                                function(x) {
-                                 applyadj(x, year, adj, tradedata, dbg)
+                                 applyadj(x, year, adj, tradedata)
                                },
                                .progress = ifelse(prog, "text", "none"),
                                .inform = FALSE,
                                .parallel = parallel))
+  tradedata <- tradedata %>%
+                 rename(orig_value=value, orig_weight=weight, orig_qty=qty)
 
+  tradedata_adj <- tradedata_adj %>%
+                     left_join(tradedata) %>%
+                     mutate(
+                            # Using coalesce to replace NAs with -1 as a NAs will expand
+                            eq_value=near(coalesce(value, -1), coalesce(orig_value, -1)),
+                            eq_weight=near(coalesce(weight, -1), coalesce(orig_weight, -1)),
+                            eq_qty=near(coalesce(qty, -1), coalesce(orig_qty, -1))
+                            )
+                     
+  tradedata_adj %>%
+    mutate(adjusted = !eq_value | !eq_weight | !eq_qty) %>%
+    select(-orig_value, -orig_weight, -orig_quantity,
+           -eq_value, -eq_weight, -eq_qty)
 }

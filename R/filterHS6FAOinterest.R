d950d4a7f3e1244a9875843cb909b68998ca4370
Alexander Matrunich <alexander@matrunich.com>
2017-01-25 15:35:27 +0300

    filterHS6FAOinterest.R: logger instructions added

--- R/filterHS6FAOinterest.R
+++ R/filterHS6FAOinterest.R
@@ -4,6 +4,7 @@
 #' Such solution drops all HS codes shorter than 6 digits.
 #'
 #' @import dplyr
+#' @import futile.logger
 #' @import stringr
 #' @export
 
@@ -13,11 +14,22 @@ filterHS6FAOinterest <- function(tradedata) {
 
   hs6faointerest <- getAgriHSCodes()
 
+  orignrow <- nrow(tradedata)
+
+  flog.info("Filtering by HS6 agri codes.")
+  flog.info("Records before filtering: %s", orignrow)
+
   tradedata <- tradedata %>%
     mutate_(hs6 = ~str_extract(hs, "^\\d{6}")) %>%
     filter_(~!is.na(hs6)) %>%
     filter_(~hs6 %in% hs6faointerest) %>%
     select_(~-hs6)
+
+  flog.info("Records after filtering: %s",
+            nrow(tradedata))
+
+  flog.info("Share of records kept after filtering: %s",
+            scales::percent(nrow(tradedata)/orignrow))
 
   tradedata
 

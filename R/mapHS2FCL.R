8172a642f98185a4f5e723134261525c31c12b03
Alexander Matrunich <alexander@matrunich.com>
2017-02-16 16:52:59 +0300

    R/mapHS2FCL.R added

---
+++ R/mapHS2FCL.R
@@ -0,0 +1,18 @@
+#' Function to map HS codes to FCL codes
+#'
+#' @import dplyr
+#' @export
+
+mapHS2FCL <- function(tradedata, maptable, parallel = NULL) {
+
+  # Extract unique input combinations ####
+
+  # Align HS codes from data and table ####
+
+  # Find mappings ####
+
+  # Choose one from multiple matches ####
+
+  # Join original trade dataset with mapping ####
+
+}

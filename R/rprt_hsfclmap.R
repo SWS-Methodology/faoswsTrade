2deb0e5a141606440aa004521afde698ec6c6b38
Alexander Matrunich <alexander@matrunich.com>
2017-02-22 18:33:11 +0300

    rprt_hsfclmap.R created

---
+++ R/rprt_hsfclmap.R
@@ -0,0 +1,26 @@
+#' Function to produce report on HS->FCL mapping table
+#'
+#' Function calculates:
+#'
+#' \itemize{
+#'   \item Number of mapping records per country.
+#'   \item Length of HS codes.
+#'   \item Odd/even digit length of HS codes.
+#' }
+#'
+#' @param maptable Data frame with hsfclmap table.
+#' @param year Integer. Reporting year.
+#' @return TBD
+#' @import dplyr
+#' @export
+#'
+#' @seealso See \url{https://github.com/SWS-Methodology/faoswsTrade/issues/83}
+#'   for problem background.
+
+rprt_hsfclmap <- function(maptable, year) {
+
+  maptable %>% 
+    group_by_(~area) %>% 
+    summarise_(count = ~n())
+
+}

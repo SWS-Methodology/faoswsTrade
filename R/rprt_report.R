063fcc6403b4745d1fb2d36a4bd94598d4f09201
Alexander Matrunich <alexander@matrunich.com>
2017-02-24 09:37:57 +0300

    rprt_report.R created

---
+++ R/rprt_report.R
@@ -0,0 +1,16 @@
+#' Adds info to futile.logger report
+#'
+#' @param msg Object to add
+#' @param level Character. Futile logger level to use. INFO by default.
+#'
+#' @import futile.logger
+#' @export
+#'
+
+rprt_report <- function(msg, level = "info") {
+
+  fnc <- paste0("flog.", level)
+
+  do.call(fnc, msg)
+
+}

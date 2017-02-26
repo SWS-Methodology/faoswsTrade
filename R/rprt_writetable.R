5938e816f878536074a501b07a2600b1c000b7e5
Alexander Matrunich <alexander@matrunich.com>
2017-02-26 14:12:23 +0300

    rprt_writetable: comma sep added to csv output

--- R/rprt_writetable.R
+++ R/rprt_writetable.R
@@ -8,10 +8,13 @@ rprt_writetable <- function(dataset) {
   stopifnot(exists("reportdir"))
   collectreports <- exists("rprt_data")
 
+  # We want name to be the same as name of original variable
+  # in main.R code
   name <- lazyeval::expr_text(dataset)
 
   write.table(dataset,
             file = file.path(reportdir, paste0(name, ".csv")),
+            sep = ",",
             row.names = FALSE)
 
   if(collectreports) rprt_data[[name]] <<- dataset

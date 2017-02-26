dce5b9a63facd4d35c28f636548d55e06605a64f
Alexander Matrunich <alexander@matrunich.com>
2017-02-26 16:23:58 +0300

    R/rprt_report.R: proper passing of table to futile logger

--- R/rprt_report.R
+++ R/rprt_report.R
@@ -10,11 +10,15 @@
 rprt_report <- function(msg, level = "info") {
 
   fnc <- paste0("flog.", level)
+  flog_capture <- FALSE
 
   # We want pretty format for tables in log messages
-  if(any(class(msg) %in% c("tbl_df",  "tbl", "data.frame", "data.table")))
-    return(do.call(fnc, list(msg = rprt_glimpse0(msg))))
+  if(any(class(msg) %in% c("tbl_df",  "tbl", "data.frame", "data.table"))) {
+    flog_capture <- TRUE
+    if(nrow(msg) > 250)
+      return(do.call(fnc, list(msg = rprt_glimpse0(msg))))
+  }
 
-  do.call(fnc, list(msg = msg))
+  do.call(fnc, list(msg = msg, capture = flog_capture))
 
 }

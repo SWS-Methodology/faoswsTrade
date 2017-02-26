d400b4d218d1aff6e9922a8f391b625c88cdd580
Alexander Matrunich <alexander@matrunich.com>
2017-02-26 11:53:10 +0300

    R/rprt_report.R: rprt_glimpse0 is applied for data frames to have nice formatting in log

--- R/rprt_report.R
+++ R/rprt_report.R
@@ -11,6 +11,10 @@ rprt_report <- function(msg, level = "info") {
 
   fnc <- paste0("flog.", level)
 
+  # We want pretty format for tables in log messages
+  if(any(class(msg) %in% c("tbl_df",  "tbl", "data.frame", "data.table")))
+    return(do.call(fnc, list(msg = rprt_glimpse0(msg))))
+
   do.call(fnc, list(msg = msg))
 
 }

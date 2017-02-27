e38747baa607399d13e46db31bbf1accb1fbe1d5
Alexander Matrunich <alexander@matrunich.com>
2017-02-27 13:21:31 +0300

    rprt_uniqhs.R: NAs were produced not by n(), but by tidyr::spread(). Corrected

--- R/rprt_uniqhs.R
+++ R/rprt_uniqhs.R
@@ -17,8 +17,9 @@ rprt_uniqhs <- function(uniqhs, tradedataname = NULL) {
   uniq_hs_by_reporter <- uniqhs %>%
     group_by_(~reporter, ~flow) %>%
     summarize_(flow_uniq_hs = ~n()) %>%
-    mutate_(flow_uniq_hs = ~ifelse(is.na(flow_uniq_hs), 0, flow_uniq_hs)) %>%
-    tidyr::spread_(key_col = "flow", value_col = "flow_uniq_hs") %>%
+    tidyr::spread_(key_col = "flow",
+                   value_col = "flow_uniq_hs",
+                   fill = 0) %>%
     rename_(uniq_hs_flow1 = ~`1`, uniq_hs_flow2 = ~`2`) %>%
     mutate_(total_uniq_hs = ~sum(uniq_hs_flow1, uniq_hs_flow2))
 

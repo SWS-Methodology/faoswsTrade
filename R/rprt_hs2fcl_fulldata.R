4d8f19b9dc7a910e60b74e66d294a041319fe837
Alexander Matrunich <alexander@matrunich.com>
2017-03-10 14:13:36 +0300

    rprt_hs2fcl_fulldata() now saves csv files

--- R/rprt_hs2fcl_fulldata.R
+++ R/rprt_hs2fcl_fulldata.R
@@ -1,6 +1,6 @@
 #' Reporting on multiple matches during HS->FCL matching by hsInRange function.
 #'
-#' The function generates the following statistics per reporter, hs6, 
+#' The function generates the following statistics per reporter, hs6,
 #' reporter/hs6:
 #' \itemize{
 #'   \item Number of nonmatched records.
@@ -21,35 +21,40 @@ rprt_hs2fcl_fulldata <- function(tradedata, tradedataname = NULL) {
   stopifnot(!(is.null(tradedataname)))
   stopifnot(length(tradedataname) == 1L)
 
+  tradedataname <- paste0(tradedataname, "_fulldata_")
+
   if(!"nolink" %in% colnames(tradedata))
     tradedata$nolink <- is.na(tradedata$fcl) else
       stopifnot(is.logical(tradedata$nolink))
 
   if(!"hs6" %in% colnames(tradedata))
     tradedata$hs6 <- stringr::str_sub(tradedata$hs, end = 6L)
-  
+
   stats <- list(nolink_count = ~sum(nolink),
                 nolink_prop  = ~nolink_count / n(),
                 nolink_value = ~sum(value * as.integer(nolink)),
                 nolink_value_prop = ~nolink_value / sum(value))
-  
-  prepdata <- list()
-  
-  byreporter <- tradedata %>%
+
+  filters <- list(~nolink_count != 0L)
+
+  nolinks_byreporter <- tradedata %>%
     group_by_(~reporter) %>%
-    summarise_(.dots = stats)
-  
-  byreporterhs6 <- tradedata %>% 
-    group_by_(~reporter, ~hs6) %>% 
-    summarize_(.dots = stats) %>% 
-    ungroup()
-  
-  byhs6 <- tradedata %>% 
-    group_by_(~hs6) %>% 
-    summarize_(.dots = stats) 
-  
-  rprt_writetable(byreporter, prefix = tradedataname)
-  rprt_writetable(byreporterhs6, prefix = tradedataname)
-  rprt_writetable(byhs6, prefix = tradedataname)
-    
+    summarise_(.dots = stats) %>%
+    filter_(.dots = filters)
+
+  nolinks_byreporterhs6 <- tradedata %>%
+    group_by_(~reporter, ~hs6) %>%
+    summarize_(.dots = stats) %>%
+    ungroup() %>%
+    filter_(.dots = filters)
+
+  nolinks_byhs6 <- tradedata %>%
+    group_by_(~hs6) %>%
+    summarize_(.dots = stats) %>%
+    filter_(.dots = filters)
+
+  rprt_writetable(nolinks_byreporter, prefix = tradedataname)
+  rprt_writetable(nolinks_byreporterhs6, prefix = tradedataname)
+  rprt_writetable(nolinks_byhs6, prefix = tradedataname)
+
 }

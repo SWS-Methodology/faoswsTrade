e796cdee229214c84a21293d6d4d8e971a94c95a
Christian Mongeau <chrMongeau@users.noreply.github.com>
2017-04-04 11:14:22 +0200

    Add checks on esdata and tldata rows > 0

--- modules/complete_tf_cpc/main.R
+++ modules/complete_tf_cpc/main.R
@@ -377,6 +377,8 @@ esdata <- ReadDatatable(paste0("ce_combinednomenclature_unlogged_",year),
                         where = paste0("chapter IN (", hs_chapters_str, ")")
 )
 
+stopifnot(nrow(esdata) > 0)
+
 if(!is.null(samplesize)) {
   esdata <- sample_n(esdata, samplesize)
   warning(sprintf("Eurostat data was sampled with size %d", samplesize))
@@ -548,6 +550,8 @@ tldata <- ReadDatatable(paste0("ct_tariffline_unlogged_",year),
                                   "chapter"),
                         where = paste0("chapter IN (", hs_chapters_str, ")")
                         )
+
+stopifnot(nrow(tldata) > 0)
 
 ##' 1. Use standard (common) variable names (e.g., `rep` becomes `reporter`).
 

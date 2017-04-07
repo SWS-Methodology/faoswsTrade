879f69326dd2417608c8cbbbadadb5df38947c09
Alexander Matrunich <alexander@matrunich.com>
2017-04-07 18:48:16 +0400

    set_sws_dev_settings.R: local sws settings moved to function

--- modules/complete_tf_cpc/main.R
+++ modules/complete_tf_cpc/main.R
@@ -85,44 +85,7 @@ library(faoswsFlag)
 
 # Development (SWS-outside) mode addons ####
 if(faosws::CheckDebug()){
-  localsettingspath <- "modules/complete_tf_cpc/sws.yml"
-  SETTINGS <- faoswsModules::ReadSettings(localsettingspath)
-  flog.debug("Local settings read from %s",
-             localsettingspath)
-  flog.debug("Local settings read:",
-             SETTINGS,
-             capture = TRUE)
-
-  USER <- if_else(.Platform$OS.type == "unix",
-                  Sys.getenv('USER'),
-                  Sys.getenv('USERNAME'))
-
-  ## Define where your certificates are stored
-  faosws::SetClientFiles(SETTINGS[["certdir"]])
-
-  ## Get session information from SWS. Token must be obtained from web interface
-  faosws::GetTestEnvironment(baseUrl = SETTINGS[["server"]],
-                             token = SETTINGS[["token"]])
-
-  # R_SWS_SHARE_PATH: 1) environment; 2) user; 3) fallback
-  if (is.na(Sys.getenv("R_SWS_SHARE_PATH", unset = NA))) {
-    flog.debug("R_SWS_SHARE_PATH environment variable not found.")
-
-    if (!is.na(SETTINGS[['share']]) & dir.exists(SETTINGS[['share']])) {
-      flog.debug("A valid 'share' variable was found in %s",
-                 localsettingspath)
-      Sys.setenv("R_SWS_SHARE_PATH" = SETTINGS[['share']])
-      flog.debug("R_SWS_SHARE_PATH set to %s",
-                 SETTINGS[['share']])
-    } else {
-      # Fall-back R_SWS_SHARE_PATH var
-      flog.debug("An invalid/inexistent 'share' variable in %s",
-                 localsettingspath)
-      Sys.setenv("R_SWS_SHARE_PATH" = tempdir())
-      flog.debug("R_SWS_SHARE_PATH now points to R temp directory %s",
-                 tempdir())
-    }
-  }
+  set_sws_dev_settings("modules/complete_tf_cpc/sws.yml")
 } else {
     # Remove domain from username
     USER <- regmatches(

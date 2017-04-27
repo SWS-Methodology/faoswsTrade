8433aabe9c52ec49b3f7d276a4b04fe530dad536
Alexander Matrunich <alexander@matrunich.com>
2017-04-27 13:17:44 +0400

    create rprt_hs6fclmap()

---
+++ R/rprt_hs6fclmap.R
@@ -0,0 +1,34 @@
+#' Reports on results of extraction of HS6->FCL map
+#'
+#' @param data produced by extract_hs6fclmap function
+#' @import dplyr
+
+rprt_hs6fclmap <- function(data = NULL) {
+
+  stopifnot(!is.null(data))
+
+  hs6fclmap_by_reporter_stat <- data %>%
+    group_by_(~reporter, ~flow) %>%
+    mutate(total_links = n()) %>%
+    group_by_(~total_links, ~hs6, add = TRUE) %>%
+    summarise(one2one = n() == 1L) %>%
+    group_by_(~reporter, ~flow, ~total_links) %>%
+    summarise_(
+      uniq_hs6 = ~n(),
+      one2one_count = ~sum(one2one),
+      one2one_prop = ~one2one_count / uniq_hs6) %>%
+    ungroup()
+
+  hs6fclmap_total_stat <- hs6fclmap_by_reporter_stat %>%
+    summarise_at(vars(total_links, uniq_hs6, one2one_count), funs(sum)) %>%
+    mutate_(one2one_prop = ~one2one_count / uniq_hs6)
+
+  hs6fclmap_by_reporter_stat <- add_area_names(hs6fclmap_by_reporter_stat,
+                                               "fao")
+
+  rprt_writetable(hs6fclmap_by_reporter_stat)
+  rprt_writetable(hs6fclmap_total_stat)
+  rprt_fulltable(hs6fclmap_total_stat, pretty_prop = TRUE)
+  rprt_fulltable(hs6fclmap_by_reporter_stat, pretty_prop = TRUE)
+
+}

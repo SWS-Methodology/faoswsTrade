3c1db8b4e9300558e59e93651215382a757a51b2
Alexander Matrunich <alexander@matrunich.com>
2017-05-01 18:20:04 +0400

    mapHS2FCL: write csv file with all hs+ to fcl links

--- R/mapHS2FCL.R
+++ R/mapHS2FCL.R
@@ -63,6 +63,9 @@ mapHS2FCL <- function(tradedata, maptable, parallel = FALSE) {
   flog.trace("HS+ mapping: looking for links", name = "dev")
   uniqhs <- hsInRange(uniqhs, maptable, parallel = parallel)
 
+  hs2fcl_mapped_links <- uniqhs
+  rprt_writetable(hs2fcl_mapped_links, prefix = tradedataname)
+
   # Report on nolinks
   rprt_hs2fcl_nolinks(uniqhs, tradedataname = tradedataname)
 

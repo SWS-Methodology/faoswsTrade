9a0d37d7b63d0a184fee6cfe6a96f76f8891a1cc
Alexander Matrunich <alexander@matrunich.com>
2017-05-02 14:05:39 +0400

    main.R: build_id changed to master (it is used to construct directory name)

--- modules/complete_tf_cpc/main.R
+++ modules/complete_tf_cpc/main.R
@@ -37,8 +37,8 @@ knitr::opts_chunk$set(echo = FALSE, eval = FALSE)
 
 # Package build ID
 # It is included into report directory name
-build_id <- "hs6map"
-stopaftermapping <- TRUE
+build_id <- "master"
+stopaftermapping <- FALSE
 
 set.seed(2507)
 

003d483c9b1c7ec93e570224a04251d409fc5709
Alexander Matrunich <alexander@matrunich.com>
2017-06-02 15:19:39 +0400

    adaptTradeDataTypes: a workaround for flow column original type.

--- R/adaptTradeDataTypes.R
+++ R/adaptTradeDataTypes.R
@@ -31,6 +31,14 @@ adaptTradeDataTypes <- function(tradedata, origin) {
     mutate_(novalue = ~is.na(value),
             noqty   = ~is.na(weight) & is.na(qty))
 
+  # When building preprocess time series we need to bind TL and ES reports. In
+  # his local copies of raw trade data sets Alex has differences in type of
+  # flows, so we convert flow to integer before calling preprocess reports.
+  #
+  # It is a temporary workaround and can be removed if all sources have flow as
+  # a character variable.
+  tradedata$flow <- as.integer(tradedata$flow)
+
   rprt(tradedata, "rawtradedata", tradedataname)
 
   tradedata <- tradedata %>%

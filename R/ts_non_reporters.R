26908d2b3a4d0c44692102ee5d6d33ab956044cd
CaetanoVidigal <Bruno.CaetanoVidigal@fao.org>
2017-06-03 17:13:47 +0200

    Functions to produce the tables of reporters (#1) and non reporters countries (#2)

---
+++ R/ts_non_reporters.R
@@ -0,0 +1,30 @@
+#' Function to produce the list of non-reporting countries.
+#'
+#' @param collection_path String with path where single-year report directories
+#'   are stored.
+#'
+#' @param prefix String with report directory name prefix without trailing
+#'   underscore. By default NULL.
+#'
+#' @return NULL invisibly.
+#' @export
+#' @import dplyr
+#' @examples ts_non_reporters("/mnt/storage/sws_share/sas", "flows_to_mirror_raw")
+#'
+
+ts_non_reporters = function(collection_path = NULL, prefix = NULL) {
+
+  elems <- c("flows_to_mirror_raw")
+
+  extract_rprt_elem(collection_path, prefix, elems) %>%
+    bind_rprts() %>%
+    bind_rows() %>%
+    ungroup() %>%
+    select(area, flow, name, year) %>%
+    mutate(exist = 1) %>%
+    arrange(year, name, flow) %>%
+    tidyr::spread(year, exist)
+
+  invisible(NULL)
+
+}
---
+++ R/ts_reporters.R
@@ -0,0 +1,32 @@
+#' Function to produce the list of reporters cointained in the file.
+#'
+#' @param collection_path String with path where single-year report directories
+#'   are stored.
+#'
+#' @param prefix String with report directory name prefix without trailing
+#'   underscore. By default NULL.
+#'
+#' @return NULL invisibly.
+#' @export
+#' @import dplyr
+#' @examples ts_non_reporters("/mnt/storage/sws_share/sas", "esdata_hs2fcl_mapped_links")
+#'
+
+
+
+ts_reporters = function(collection_path = NULL, prefix = NULL) {
+
+  elems <- c("esdata_hs2fcl_mapped_links",
+             "tldata_hs2fcl_mapped_links")
+
+  extract_rprt_elem(collection_path, prefix, elems) %>%
+    bind_rprts() %>%
+    bind_rows() %>%
+    ungroup() %>%
+    select(reporter, flow, year) %>%
+    arrange(year, reporter, flow) %>%
+    select(year, reporter, flow)
+
+  invisible(NULL)
+
+}

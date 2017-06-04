978224689aaa30c232c05a7dc2c7e28fcee6ab87
Christian Mongeau <chrMongeau@users.noreply.github.com>
2017-06-04 10:09:42 +0200

    Add parameter for complete reports. Fix #141

--- R/ts_all_reports.R
+++ R/ts_all_reports.R
@@ -1,37 +1,35 @@
-#'General function to produce all time-series reports
-#'@param collection_path String with path where single-year report directories
-#'  are stored.
-#'@param prefix String with report directory name prefix without trailing
-#'  underscore. By default NULL.
-#'@param ts_reports_path String with path where time series reports should be
-#'  saved. If NULL (default) the directory with TS reports will be saved where
-#'  single-year reports are located (collection_path).
-#'@return NULL inivisibly.
-#'@export
-#'@import dplyr
-#'@examples ts_all_reports("/mnt/storage/sws_share/sas", "complete_tf_cpc")
-#'
+#' General function to produce all time-series reports
+#' @param collection_path String with path where single-year report directories
+#'   are stored.
+#' @param prefix String with report directory name prefix without trailing
+#'   underscore. By default NULL.
+#' @param ts_reports_path String with path where time series reports should be
+#'   saved. If NULL (default) the directory with TS reports will be saved where
+#'   single-year reports are located (collection_path).
+#' @param complete Logical If TRUE (default) all reports (raw data, mapping,
+#'   etc.) will be generated, if FALSE only raw data reports will be produced.
+#' @return NULL inivisibly.
+#' @export
+#' @import dplyr
+#' @examples ts_all_reports("/mnt/storage/sws_share/sas", "complete_tf_cpc")
 
 ts_all_reports <- function(collection_path = NULL,
                            prefix = NULL,
-                           ts_reports_path = NULL) {
+                           ts_reports_path = NULL,
+                           complete = TRUE) {
 
   stopifnot(!is.null(collection_path))
   stopifnot(!is.null(prefix))
   stopifnot(dir.exists(collection_path))
+  stopifnot(is.logical(complete))
 
   if (!is.null(ts_reports_path)) {
-    if (!dir.exists(ts_reports_path))
-      dir.create(ts_reports_path,
-                 recursive = TRUE,
-                 showWarnings = FALSE)
-      } else ts_reports_path <- collection_path
-
-  # ts_hsfcl_nolinks(collection_path, prefix = prefix) %>%
-  #   ts_write_rprt(ts_reports_path, "ts_hsfcl_nolinks")
-#
-#   ts_hsfcl_nolinks_statistic(collection_path, prefix = prefix) %>%
-#     ts_write_rprt(ts_reports_path, "ts_hsfcl_nolinks_statistics")
+    if (!dir.exists(ts_reports_path)) {
+      dir.create(ts_reports_path, recursive = TRUE, showWarnings = FALSE)
+    }
+  } else {
+    ts_reports_path <- collection_path
+  }
 
   ts_preproc(collection_path, prefix = prefix) %>%
     ts_write_rprt(ts_reports_path, "ts_preproc_long_format")
@@ -42,5 +40,13 @@ ts_all_reports <- function(collection_path = NULL,
   ts_non_reporters(collection_path, prefix = prefix) %>%
     ts_write_rprt(ts_reports_path, "ts_non_reporters")
 
-invisible(NULL)
+  if (complete) {
+    ts_hsfcl_nolinks(collection_path, prefix = prefix) %>%
+      ts_write_rprt(ts_reports_path, "ts_hsfcl_nolinks")
+
+    ts_hsfcl_nolinks_statistic(collection_path, prefix = prefix) %>%
+      ts_write_rprt(ts_reports_path, "ts_hsfcl_nolinks_statistics")
+  }
+
+  invisible(NULL)
 }

576bf69eec8480db92749be2f487471feab9217b
Christian Mongeau <chrMongeau@users.noreply.github.com>
2017-07-27 18:24:21 +0200

    Remove invalid reporters. Fix #128

--- modules/complete_tf_cpc/main.R
+++ modules/complete_tf_cpc/main.R
@@ -317,6 +317,34 @@ tldata <- tldata %>%
     partner = ~as.integer(faoswsTrade::convertComtradeM49ToFAO(m49par))
   )
 
+# Create a table with valid reporters
+valid_reporters <-
+  GetCodeList(
+    domain    = 'faostat_one',
+    dataset   = 'FS1_SUA_UPD',
+    dimension = 'geographicAreaFS'
+  ) %>%
+  filter(type != 'group') %>%
+  mutate(
+    startDate = as.numeric(stringr::str_sub(startDate, 1, 4)),
+    endDate   = as.numeric(stringr::str_sub(endDate, 1, 4))
+  ) %>%
+  select(-selectionOnly, -type) %>%
+  filter(startDate <= year, endDate >= year)
+
+reporters_to_drop <- setdiff(unique(tldata$reporter), valid_reporters$code)
+
+if (length(reporters_to_drop) > 0) {
+  flog.trace("TL: dropping invalid reporters", name = "dev")
+  x <- ifelse(length(reporters_to_drop) == 1, 'y', 'ies')
+  warning(paste0('The following countr', x, ' did not exist in ', year, ': ',
+                paste(reporters_to_drop, collapse=' ')))
+
+  # Keep valid reporters
+  tldata <- tldata %>%
+    filter(reporter %in% valid_reporters$code)
+}
+
 flog.trace("TL: dropping reporters already found in Eurostat data", name = "dev")
 # They will be replaced by ES data
 tldata <- tldata %>%

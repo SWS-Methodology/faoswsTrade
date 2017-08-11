98f97d6909fc5e9e0f8a8d5c161c5444ef0e30ca
Christian Mongeau <chrMongeau@users.noreply.github.com>
2017-08-11 17:42:08 +0200

    Use data.table::foverlaps for matching ranges. Issue #146

--- R/hsInRange.R
+++ R/hsInRange.R
@@ -13,91 +13,28 @@
 #'   there were no matching FCL codes, NA in fcl column is returned.
 #'
 #' @import dplyr
+#' @import data.table
 #' @export
 
 
-hsInRange <- function(uniqhs,
-                      maptable,
-                      parallel = FALSE) {
+hsInRange <- function(uniqhs, maptable, parallel = FALSE) {
+
+  # Looking for matching fcl codes for each input hs code.
+  # If there are multiple matchings we return all matches.
+  # dataframes need to be transformed to data.table format
+  # and the RHS data.table needs keys.
 
   df <- uniqhs %>%
-    mutate(datumid = row_number())
+    mutate(datumid = row_number()) %>%
+    mutate(fromcodeext = hsext, tocodeext = hsext) %>%
+    as.data.table()
 
-  # Splitting of trade dataset by area and flow
-  # and applying mapping function to each part
-  df_fcl <- plyr::ddply(
-    df,
-    .variables = c("reporter", "flow"),
-    .fun = function(subdf) {
+  maptable <- as.data.table(maptable)
+  
+  setkey(maptable, reporter, flow, fromcodeext, tocodeext)
 
-      # Subsetting mapping file
-      maptable <- maptable %>%
-        filter_(~reporter == subdf$reporter[1],
-                ~flow == subdf$flow[1])
+  foverlaps(df, maptable) %>%
+    tbl_df() %>%
+    select(reporter, flow, datumid, hs, hsext, fcl, recordnumb)
 
-      # If no corresponding records in map return empty df
-      if(nrow(maptable) == 0)
-        return(data_frame(
-          datumid = subdf$id,
-          hs = subdf$hs,
-          hsext = subdf$hsext,
-          fcl = as.integer(NA),
-          recordnumb = as.numeric(NA)))
-
-
-      # Split original data.frame by row,
-      # and looking for matching fcl codes
-      # for each input hs code.
-      # If there are multiple matchings we return
-      # all matches.
-      fcl <- plyr::ldply(
-        subdf$datumid,
-        function(currentid) {
-
-          # Put single hs code into a separate variable
-          hsext <- subdf %>%
-            filter_(~datumid == currentid) %>%
-            select_(~hsext) %>%
-            unlist() %>% unname()
-
-          # Original HS to include into output dataset
-          hs <- subdf %>%
-            filter_(~datumid == currentid) %>%
-            select_(~hs) %>%
-            unlist() %>% unname()
-
-          maptable <- maptable %>%
-            filter_(~fromcodeext <= hsext &
-                      tocodeext >= hsext)
-
-          # If no corresponding HS range is
-          # available return empty integer
-          if(nrow(maptable) == 0L) {
-            fcl <- as.integer(NA)
-            recordnumb <- as.numeric(NA)
-          }
-
-          if(nrow(maptable) >= 1L) {
-             fcl <- maptable$fcl
-             recordnumb <- maptable$recordnumb
-          }
-
-          data_frame(datumid = currentid,
-                     hs = hs,
-                     hsext = hsext,
-                     fcl = fcl,
-                     recordnumb = recordnumb)
-        }
-      )
-
-    },
-    .parallel = parallel,
-    # Windows requires functions and packages to be explicitly exported
-    .paropts = list(.packages = "dplyr"),
-    .progress = ifelse(CheckDebug() &
-                         !parallel &
-                         # Don't show progress for quick calculations
-                         nrow(uniqhs) > 10^4,
-                       "text", "none")
-  )
 }

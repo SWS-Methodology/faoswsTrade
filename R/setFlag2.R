ab6f9f986d6182b4143406dd0f4cbf64dcdff2c4
Christian Mongeau <chrMongeau@users.noreply.github.com>
2017-08-29 10:45:58 +0200

    Fix setFlagX so that it allows T Status flag

--- R/setFlag2.R
+++ R/setFlag2.R
@@ -32,8 +32,8 @@ setFlag2 <- function(data = NA, condition = NA, type = NA, flag = NA, variable =
 
   if (missing(flag)) {
     stop("Please, set 'flag' to one flag value.")
-  } else if (type == 'status' & !(flag %in% c('E', 'I', 'X'))) {
-    stop(paste("'flag' should be one of 'E', 'I', 'X'"))
+  } else if (type == 'status' & !(flag %in% c('T', 'E', 'I', 'X'))) {
+    stop(paste("'flag' should be one of 'T', 'E', 'I', 'X'"))
   } else if (type == 'method' & !(flag %in% c('c', 'i', 'e', 's', 'h'))) {
     stop(paste("'flag' should be one of 'c', 'i', 'e', 's', 'h'"))
   }

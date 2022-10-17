09108869a3395db23f0b22586cea24f94af1a60e
aydans <aydanselek@gmail.com>
2022-10-17 14:27:29 +0200

    Clean

--- modules/top_commodities_selection_routine/main.R
+++ modules/top_commodities_selection_routine/main.R
@@ -14,7 +14,7 @@
 ##'
 ##' None
 
-
+message("TRADE: Top commodities selection routine starts...")
 
 ## Load the libraries
 library(faosws)
@@ -22,6 +22,8 @@ library(data.table)
 library(faoswsUtil)
 library(sendmailR)
 library(openxlsx)
+library(futile.logger)
+suppressPackageStartupMessages(library(dplyr, warn.conflicts = FALSE))
 
 
 send_mail <- function(from = NA, to = NA, subject = NA,
@@ -93,45 +95,17 @@ send_mail <- function(from = NA, to = NA, subject = NA,
   sendmailR::sendmail(from, to, subject, as.list(body))
 }
 
-# R_SWS_SHARE_PATH = Sys.getenv("R_SWS_SHARE_PATH")
-#
-# # ## set up for the test environment and parameters
-# # R_SWS_SHARE_PATH = Sys.getenv("R_SWS_SHARE_PATH")
-#
-# if(CheckDebug()){
-#   message("Not on server, so setting up environment...")
-#
-#   library(faoswsModules)
-#   SETT <- ReadSettings("C:/Users/Selek/Dropbox/1-FAO-DROPBOX/faosws_Trade/modules/top_commodities_selection_routine/sws.yml")
-#
-#   R_SWS_SHARE_PATH <- SETT[["share"]]
-#   ## Get SWS Parameters
-#   SetClientFiles(dir = SETT[["certdir"]])
-#   GetTestEnvironment(
-#     baseUrl = SETT[["server"]],
-#     token = SETT[["token"]]
-#   )
-# }
-
+sapply(dir("R", full.names = TRUE), source)
 
 R_SWS_SHARE_PATH <- Sys.getenv("R_SWS_SHARE_PATH")
-
+dev_sws_set_file <- "modules/top_commodities_selection_routine/sws.yml"
 if (CheckDebug()) {
-  R_SWS_SHARE_PATH <- "//hqlprsws1.hq.un.fao.org/sws_r_share"
-
-  mydir <- "/Users/aydanselek/Dropbox/1-FAO-DROPBOX/faosws_Trade/modules/top_commodities_selection_routine"
-
-  SETTINGS <- faoswsModules::ReadSettings(file.path(mydir, "sws.yml"))
-
-  SetClientFiles(dir='C:/Users/aydan/Desktop/qa')
-
-  GetTestEnvironment(baseUrl = 'https://hqlqasws1.hq.un.fao.org:8181/sws',token = '552f1302-37fe-477c-a8cd-a6a4b1df191f')
+  set_sws_dev_settings(dev_sws_set_file)
 }
 
 `%!in%` = Negate(`%in%`)
 
 year = as.numeric(swsContext.computationParams$year)
-# year <- as.numeric(2018)
 
 # the average will be calculated with the previous 5 years
 interval <- (year-1):(year-5)
@@ -145,12 +119,6 @@ USER <- regmatches(
 )
 
 COUNTRY <- as.character(swsContext.datasets[[1]]@dimensions$geographicAreaM49@keys)
-
-# Create temporary location for the output
-TMP_DIR <- file.path(tempdir())
-if (!file.exists(TMP_DIR)) dir.create(TMP_DIR, recursive = TRUE)
-tmp_file_tpselection <- file.path(TMP_DIR, paste0("TP_selections_", COUNTRY, ".xlsx"))
-
 
 # Get data configuration and session
 sessionKey = swsContext.datasets[[1]]
@@ -169,12 +137,6 @@ itemKeys = itemKeys[, code]
 
 eleKeys <- GetCodeList(domain = "trade", dataset = "total_trade_cpc_m49", "measuredElementTrade")
 eleKeys <- eleKeys[, code]
-
-#########################################
-#####   Pull data from total trade  #####
-#########################################
-
-message("TradeOUT: Pulling trade Data")
 
 # Define geo dimension
 geoDim = Dimension(name = "geographicAreaM49", keys = selectedGEOCode)
@@ -202,12 +164,17 @@ key = DatasetKey(domain = "trade", dataset = "total_trade_cpc_m49", dimensions =
 # The actual values will be used to show which figures have been deleted by the TP selection plug-in.
 # It is only to easy the job of country analyst.
 
-actual_value_current = readRDS(paste0(R_SWS_SHARE_PATH, '/trade/validation_tool_files/tp_criterion/', year, '.rds'))
-actual_value_1 = readRDS(paste0(R_SWS_SHARE_PATH,'/trade/validation_tool_files/tp_criterion/', (year-1), '.rds'))
-actual_value_2 = readRDS(paste0(R_SWS_SHARE_PATH,'/trade/validation_tool_files/tp_criterion/', (year-2), '.rds'))
-actual_value_3 = readRDS(paste0(R_SWS_SHARE_PATH,'/trade/validation_tool_files/tp_criterion/', (year-3), '.rds'))
-actual_value_4 = readRDS(paste0(R_SWS_SHARE_PATH,'/trade/validation_tool_files/tp_criterion/', (year-4), '.rds'))
-# actual_value_5 = readRDS(paste0('R:/trade/validation_tool_files/tp_criterion/', (year-5), '.rds'))
+actual_value_current = setDT(readRDS(paste0(R_SWS_SHARE_PATH, '/trade/validation_tool_files/tp_criterion/', year, '.rds')))
+actual_value_1 = setDT(readRDS(paste0(R_SWS_SHARE_PATH,'/trade/validation_tool_files/tp_criterion/', (year-1), '.rds')))
+actual_value_2 = setDT(readRDS(paste0(R_SWS_SHARE_PATH,'/trade/validation_tool_files/tp_criterion/', (year-2), '.rds')))
+actual_value_3 = setDT(readRDS(paste0(R_SWS_SHARE_PATH,'/trade/validation_tool_files/tp_criterion/', (year-3), '.rds')))
+actual_value_4 = setDT(readRDS(paste0(R_SWS_SHARE_PATH,'/trade/validation_tool_files/tp_criterion/', (year-4), '.rds')))
+
+actual_value_current_value <- copy(actual_value_current)
+setDT(actual_value_current_value)
+actual_value_current_value <- actual_value_current_value[(measuredElementTrade == 5622 | measuredElementTrade == 5922) & geographicAreaM49Reporter == COUNTRY,
+                                             .(Value = sum(Value)), by = .(timePointYears, geographicAreaM49Reporter,measuredItemCPC, measuredElementTrade)]
+######################
 
 actual_value_current <- actual_value_current[(measuredElementTrade == 5610 | measuredElementTrade == 5910) & geographicAreaM49Reporter == COUNTRY,
                                             .(Value = sum(Value)), by = .(timePointYears, geographicAreaM49Reporter,measuredItemCPC, measuredElementTrade)]
@@ -221,11 +188,9 @@ actual_value_3 <- actual_value_3[(measuredElementTrade == 5610 | measuredElement
                                             .(Value = sum(Value)), by = .(timePointYears, geographicAreaM49Reporter,measuredItemCPC, measuredElementTrade)]
 actual_value_4 <- actual_value_4[(measuredElementTrade == 5610 | measuredElementTrade == 5910) & geographicAreaM49Reporter == COUNTRY,
                                             .(Value = sum(Value)), by = .(timePointYears, geographicAreaM49Reporter,measuredItemCPC, measuredElementTrade)]
-# actual_value_5 <- actual_value_5[(measuredElementTrade == 5610 | measuredElementTrade == 5910) & geographicAreaM49Reporter == COUNTRY,
-#                                              .(Value = sum(Value)), by = .(timePointYears, geographicAreaM49Reporter,measuredItemCPC, measuredElementTrade)]
 
 actual_value_total <- do.call("rbind", list(actual_value_current, actual_value_1, actual_value_2, actual_value_3, actual_value_4))
-# actual_value_total$timePointYears <- paste0(actual_value_total$timePointYears, '_actual')
+
 if (nrow(actual_value_total)>0){
   actual_value_total <- dcast.data.table(actual_value_total, geographicAreaM49Reporter + measuredItemCPC + measuredElementTrade
                                          ~ timePointYears, value.var = list('Value'))
@@ -236,8 +201,6 @@ setnames(actual_value_total, 'geographicAreaM49Reporter', 'geographicAreaM49')
 
 ####
 non_reporting_countries <- ReadDatatable("ess_trade_apply_tp_criterion")
-
-# stopifnot(sessionCountries %in% non_reporting_countries$area)
 
 data = GetData(key, omitna = FALSE, normalized = FALSE)
 data = normalise(data, areaVar = "geographicAreaM49",
@@ -295,57 +258,113 @@ official_data3 <- official_data3[order(official_data3$id), ]
 official_data3[,id:=NULL]
 
 ### Assign the actual values ###
-outList_to_actuals <- outList_final[,.(geographicAreaM49, geographicAreaM49_description, measuredItemCPC,
-                                        measuredItemCPC_description, measuredElementTrade,
-                                        measuredElementTrade_description)]
-outList_to_actuals$id <- 1:nrow(outList_to_actuals)
+if (nrow(actual_value_total)>0){
+  outList_to_actuals <- outList_final[,.(geographicAreaM49, geographicAreaM49_description, measuredItemCPC,
+                                         measuredItemCPC_description, measuredElementTrade,
+                                         measuredElementTrade_description)]
+  outList_to_actuals$id <- 1:nrow(outList_to_actuals)
 
-actual_value_total$geographicAreaM49 <- as.character(actual_value_total$geographicAreaM49)
-actual_value_total$measuredElementTrade <- as.character(actual_value_total$measuredElementTrade)
+  actual_value_total$geographicAreaM49 <- as.character(actual_value_total$geographicAreaM49)
+  actual_value_total$measuredElementTrade <- as.character(actual_value_total$measuredElementTrade)
 
 
-actual_value_total2 <- merge(outList_to_actuals, actual_value_total, by=c('geographicAreaM49', 'measuredItemCPC',
-                                                                          'measuredElementTrade'), all.x=T, all.y=F)
+  actual_value_total2 <- merge(outList_to_actuals, actual_value_total, by=c('geographicAreaM49', 'measuredItemCPC',
+                                                                            'measuredElementTrade'), all.x=T, all.y=F)
 
-actual_value_total2 <- actual_value_total2[order(actual_value_total2$id), ]
+  actual_value_total2 <- actual_value_total2[order(actual_value_total2$id), ]
 
-actual_value_total2[,id:=NULL]
+  actual_value_total2[,id:=NULL]
 
-idvars = c("geographicAreaM49", "measuredItemCPC", "measuredElementTrade", "geographicAreaM49_description",
-                     "measuredItemCPC_description","measuredElementTrade_description")
+  idvars = c("geographicAreaM49", "measuredItemCPC", "measuredElementTrade", "geographicAreaM49_description",
+             "measuredItemCPC_description","measuredElementTrade_description")
 
-outList_final2 <- melt.data.table(outList_final, id.vars = idvars,
-                                  measure.vars = c(names(outList_final)[names(outList_final) %!in% idvars]), variable.name = 'timePointYears', value.name = 'Value')
+  outList_final2 <- melt.data.table(outList_final, id.vars = idvars,
+                                    measure.vars = c(names(outList_final)[names(outList_final) %!in% idvars]), variable.name = 'timePointYears', value.name = 'Value')
 
-actual_value_total2 <- melt.data.table(actual_value_total2, id.vars = idvars,
-                                  measure.vars = c(names(actual_value_total2)[names(actual_value_total2) %!in% idvars]), variable.name = 'timePointYears', value.name = 'Value')
+  actual_value_total2 <- melt.data.table(actual_value_total2, id.vars = idvars,
+                                         measure.vars = c(names(actual_value_total2)[names(actual_value_total2) %!in% idvars]), variable.name = 'timePointYears', value.name = 'Value')
 
-outList_final3 <- merge(outList_final2, actual_value_total2, by = c(idvars, 'timePointYears'), all = TRUE )
+  outList_final3 <- merge(outList_final2, actual_value_total2, by = c(idvars, 'timePointYears'), all = TRUE )
 
-outList_final3 <- outList_final3[is.na(Value.x), Value.x:=Value.y]
-outList_final3 <- outList_final3[, Value.y := NULL]
-setnames(outList_final3, 'Value.x', 'Value')
+  outList_final3 <- outList_final3[is.na(Value.x), Value.x:=Value.y]
+  outList_final3 <- outList_final3[, Value.y := NULL]
+  setnames(outList_final3, 'Value.x', 'Value')
 
-outList_final4 <- dcast.data.table(outList_final3, geographicAreaM49 + geographicAreaM49_description + measuredItemCPC +
-                             measuredItemCPC_description + measuredElementTrade + measuredElementTrade_description
-                           ~ timePointYears, value.var = list('Value'))
+  outList_final4 <- dcast.data.table(outList_final3, geographicAreaM49 + geographicAreaM49_description + measuredItemCPC +
+                                       measuredItemCPC_description + measuredElementTrade + measuredElementTrade_description
+                                     ~ timePointYears, value.var = list('Value'))
 
-ordering <- outList_final[,.(geographicAreaM49, geographicAreaM49_description, measuredItemCPC,
-                                        measuredItemCPC_description, measuredElementTrade,
-                                        measuredElementTrade_description)]
-ordering$id <- 1:nrow(ordering)
+  ordering <- outList_final[,.(geographicAreaM49, geographicAreaM49_description, measuredItemCPC,
+                               measuredItemCPC_description, measuredElementTrade,
+                               measuredElementTrade_description)]
+  ordering$id <- 1:nrow(ordering)
 
-outList_final4 <- merge(ordering, outList_final4, by=c('geographicAreaM49', 'geographicAreaM49_description', 'measuredItemCPC',
-                                                                  'measuredItemCPC_description', 'measuredElementTrade',
-                                                                  'measuredElementTrade_description'), all.x=T, all.y=F)
+  outList_final4 <- merge(ordering, outList_final4, by=c('geographicAreaM49', 'geographicAreaM49_description', 'measuredItemCPC',
+                                                         'measuredItemCPC_description', 'measuredElementTrade',
+                                                         'measuredElementTrade_description'), all.x=T, all.y=F)
 
-outList_final4 <- outList_final4[order(outList_final4$id), ]
+  outList_final4 <- outList_final4[order(outList_final4$id), ]
 
-outList_final4[,id:=NULL]
+  outList_final4[,id:=NULL]
 
-###
+  ###
 
-outList_final5 <- outList_final4[, measuredItemCPC := paste0("'", measuredItemCPC)]
+
+  #### ADD DELETED VALUE COLUMN #####
+  setnames(actual_value_current_value, 'geographicAreaM49Reporter', 'geographicAreaM49')
+  actual_value_current_value[, flow:= substr(measuredElementTrade, 1, 2)]
+  actual_value_current_value[, measuredElementTrade:= NULL]
+  if (nrow(actual_value_current_value)>0){
+    actual_value_current_value[timePointYears==year, timePointYears_dollar:= paste0(year, '_Dollar_value')]
+    actual_value_current_value[,timePointYears:= NULL]
+    actual_value_current_value <- dcast.data.table(actual_value_current_value, geographicAreaM49 + measuredItemCPC + flow
+                                                    ~ timePointYears_dollar, value.var = list('Value'))
+  }
+
+
+
+  outList_final5 <- outList_final4[, flow:= substr(measuredElementTrade, 1, 2)]
+
+  if (nrow(actual_value_current_value)>0){
+    outList_final6 <- merge(outList_final5, actual_value_current_value,
+                            by = c('geographicAreaM49', 'measuredItemCPC', 'flow'), all.x = TRUE)
+  } else {
+    outList_final6 <- outList_final5
+  }
+
+
+
+  outList_final6 <- outList_final6[, flow:=NULL]
+
+
+  ordering <- outList_final[,.(geographicAreaM49, geographicAreaM49_description, measuredItemCPC,
+                               measuredItemCPC_description, measuredElementTrade,
+                               measuredElementTrade_description)]
+  ordering$id <- 1:nrow(ordering)
+
+  outList_final7 <- merge(ordering, outList_final6, by=c('geographicAreaM49', 'geographicAreaM49_description', 'measuredItemCPC',
+                                                         'measuredItemCPC_description', 'measuredElementTrade',
+                                                         'measuredElementTrade_description'), all.x=T, all.y=F)
+
+  outList_final7 <- outList_final7[order(outList_final7$id), ]
+
+  outList_final7[,id:=NULL]
+
+  outList_final5 <- outList_final7
+
+} else {
+  outList_final5 <- outList_final
+}
+
+
+name_of_country <- unique(outList_final5$geographicAreaM49_description)
+# outList_final5 <- outList_final4[, measuredItemCPC := paste0("'", measuredItemCPC)]
+
+# Create temporary location for the output
+TMP_DIR <- file.path(tempdir())
+if (!file.exists(TMP_DIR)) dir.create(TMP_DIR, recursive = TRUE)
+tmp_file_tpselection <- file.path(TMP_DIR, paste0(name_of_country, "_main_commodities.xlsx"))
+
 
 #### DESIGN THE EXCEL FILE #####
 
@@ -361,6 +380,7 @@ first_fill <- createStyle(fgFill = "red")
 style_comma <- createStyle(numFmt = "COMMA")
 # very_small <- createStyle(borderColour = "blue", borderStyle = "double", border = "TopBottomLeftRight")
 very_small <- createStyle(fgFill = "yellow")
+very_big <- createStyle(fgFill = "deepskyblue")
 # for (i in nrow(outList_final5)) {
 #    addStyle(wb, "main_commodities", cols = 13, rows = 1 + c((1:nrow(outList_final))[is.na(outList_final[[12]])]), style = first_fill, gridExpand = TRUE)
 # }
@@ -368,6 +388,11 @@ very_small <- createStyle(fgFill = "yellow")
 for (i in c(7,8,9,10,11,12)) {
       addStyle(wb, "main_commodities", cols = i, rows = 1 + c(na.omit((1:nrow(outList_final))[outList_final[[i]]/ outList_final$`5_year_average` < 0.5])), style = very_small, gridExpand = TRUE, stack = TRUE)
 }
+
+for (i in c(7,8,9,10,11,12)) {
+  addStyle(wb, "main_commodities", cols = i, rows = 1 + c(na.omit((1:nrow(outList_final))[outList_final[[i]]/ outList_final$`5_year_average` > 2])), style = very_big, gridExpand = TRUE, stack = TRUE)
+}
+
 #
 # for (i in c(7,8,9,10,11)) {
 #   addStyle(wb, "main_commodities", cols = i + 1, rows = 1 + c((1:nrow(outList_final))[is.na(outList_final[[i]])]), style = second_fill, gridExpand = TRUE)
@@ -381,7 +406,7 @@ for (i in c(7,8,9,10,11,12)) {
   addStyle(wb, "main_commodities", cols = i, rows = 1 + c(na.omit((1:nrow(outList_final))[official_data3[[i]]])), style = official, gridExpand = TRUE, stack = TRUE)
 }
 
-for (i in c(7,8,9,10,11,12,13)) {
+for (i in c(7,8,9,10,11,12,13,14)) {
   addStyle(wb, "main_commodities", cols = i, rows = 1:nrow(outList_final5)+1, style = style_comma, gridExpand = TRUE, stack = TRUE)
 }
 
@@ -389,15 +414,15 @@ for (i in c(7,8,9,10,11,12,13)) {
 
 
 saveWorkbook(wb, tmp_file_tpselection, overwrite = TRUE)
-# saveWorkbook(wb, file = 'C:/Users/aydan/Desktop/dene5.xlsx', overwrite = TRUE)
 bodyTPSelection = paste("Plugin completed. The attached excel file contains a list of main commodities.
                         ######### Figures description #########
                         Red figures: Missing or deleted bad Tp values;
                         Yellow figures: Values minimum 50% less than the 5 year average;
+                        Blue figures: Values minimum twice more than the 5-year average;
                         Bold figures: Official data.
                         ",
                     sep='\n')
 
-send_mail(from = "no-reply@fao.org", subject = "main_commodities", body = c(bodyTPSelection, tmp_file_tpselection), remove = TRUE)
+send_mail(from = "no-reply@fao.org", subject = paste("Main Commodities of ", name_of_country), body = c(bodyTPSelection, tmp_file_tpselection), remove = TRUE)
 
-print('Plug-in Completed')
+print('Plug-in Completed. Please see your e-mail.')

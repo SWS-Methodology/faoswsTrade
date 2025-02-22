991246ee3188681c77d30b20d552b9a3baa9e2f7
Cristina Muschitiello <Cristina.Muschitiello@fao.org>
2025-02-22 15:44:41 +0100

    cancel_protected_flag fixed wirh r423

--- modules/cancel_protected_flag/main.R
+++ modules/cancel_protected_flag/main.R
@@ -17,14 +17,14 @@
 message("cancel protected flags")
 
 library(data.table)
-# library(faoswsTrade)
+library(faoswsTrade)
 library(faosws)
 library(stringr)
 library(scales)
 library(faoswsUtil)
 library(openxlsx)
 library(faoswsFlag)
-# library(tidyr)
+library(tidyr)
 library(dplyr, warn.conflicts = FALSE)
 
 if (CheckDebug()) {
@@ -187,7 +187,7 @@ saveWorkbook(wb, tmp_file_cancelflag, overwrite = TRUE)
 
 bodyCancelFlag = paste("Plugin completed.")
 
-send_mail(from = "no-reply@fao.org", subject = "trade_cancel_flag", body = c(bodyCancelFlag, tmp_file_cancelflag), remove = TRUE)
+# send_mail(from = "no-reply@fao.org", subject = "trade_cancel_flag", body = c(bodyCancelFlag, tmp_file_cancelflag), remove = TRUE)
 
 
 sessionKey = swsContext.datasets[[1]]

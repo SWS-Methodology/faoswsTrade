abc69f881a9755d9b73d2822ca8fbc2beca13a13
Cristina Muschitiello <Cristina.Muschitiello@fao.org>
2025-02-22 15:22:14 +0100

    GITPLUGIN_RENV version for cancel_protected_flags first commit

--- modules/cancel_protected_flag/main.R
+++ modules/cancel_protected_flag/main.R
@@ -17,21 +17,21 @@
 message("cancel protected flags")
 
 library(data.table)
-library(faoswsTrade)
+# library(faoswsTrade)
 library(faosws)
 library(stringr)
 library(scales)
 library(faoswsUtil)
 library(openxlsx)
 library(faoswsFlag)
-library(tidyr)
+# library(tidyr)
 library(dplyr, warn.conflicts = FALSE)
 
 if (CheckDebug()) {
   library(faoswsModules)
-  SETTINGS = ReadSettings("modules/cancel_protected_flag/sws.yml")
+  SETTINGS = ReadSettings("sws.yml")
   ## Define where your certificates are stored
-  faosws::SetClientFiles(SETTINGS[["certdir"]])
+  # faosws::SetClientFiles(SETTINGS[["certdir"]]) ### Deprecated
   ## Get session information from SWS. Token must be obtained from web interface
   GetTestEnvironment(baseUrl = SETTINGS[["server"]],
                      token = SETTINGS[["token"]])
